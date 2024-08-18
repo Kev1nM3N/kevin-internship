15ab85703f9803712ff4666d800fd7e356ef2303
Kevin Mendez <mediaman502@aol.com>
2024-08-18 16:05:23 -0400

    passed API call

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
 import Home from "./pages/Home";
+import axios from "axios";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Explore from "./pages/Explore";
 import Author from "./pages/Author";
 import ItemDetails from "./pages/ItemDetails";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
+import { useState, useEffect } from "react";
+//https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections
 
 function App() {
+  const [cards, setCards] = useState([]);
+
+  async function getCards() {
+    const fetchCards = await axios.get('https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections');
+    let receiveCards = fetchCards.data;
+    return receiveCards;
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      const receiveCards = await getCards();
+      setCards(receiveCards);
+    }
+    fetchData();
+  }, []);
+
+
   return (
     <Router>
       <Nav />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home cards={cards} />} />
         <Route path="/explore" element={<Explore />} />
         <Route path="/author" element={<Author />} />
         <Route path="/item-details" element={<ItemDetails />} />
--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
 
-const HotCollections = () => {
+const HotCollections = ({ cards }) => {
+  console.log(cards);
+
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,25 +14,25 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+          {cards.map((card) => (
+            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={card?.id}>
               <div className="nft_coll">
                 <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
+                  <Link to={`/item-details/${card?.nftId}`}>
+                    <img src={card?.nftImage} className="lazy img-fluid" alt="" />
                   </Link>
                 </div>
                 <div className="nft_coll_pp">
                   <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
+                    <img className="lazy pp-coll" src={card?.authorImage} alt="" />
                   </Link>
                   <i className="fa fa-check"></i>
                 </div>
                 <div className="nft_coll_info">
                   <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
+                    <h4>{card?.title}</h4>
                   </Link>
-                  <span>ERC-192</span>
+                  <span>ERC-{card?.code}</span>
                 </div>
               </div>
             </div>
