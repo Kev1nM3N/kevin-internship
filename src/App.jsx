a6f785052ac2b54f6f136ad509b176ec7a09500d
Kevin Mendez <mediaman502@aol.com>
2024-10-03 16:11:20 -0400

    removed comment

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import ItemDetails from "./pages/ItemDetails";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import { useState, useEffect } from "react";
-//new comment test
 
 function App() {
   const [cards, setCards] = useState([]);
